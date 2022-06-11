bd9ab0762e95c01b8d94077be9f511c066807266
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-11 21:44:41 +0530

    init with t3 stack but with windi

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  webpack(config) {
+    config.plugins.push(new WindiCSSWebpackPlugin());
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
